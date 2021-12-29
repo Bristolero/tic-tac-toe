6984a2cb5876ae24774cbdce9939a09e001001bc
Bristolero <lennard.rohr@tu-dortmund.de>
2021-12-29 17:25:44 +0100

    almost done

---
+++ script.js
@@ -0,0 +1,120 @@
+//Functions
+
+const PlayerFactory = (name) => { 
+    return {name};
+}
+
+const player1 = PlayerFactory("Player 1");
+const player2 = PlayerFactory("Player 2");
+
+const GameBoard = (() => {
+    const board = ["","","","","","","","",""];
+
+    const removeListeners = () => {
+        var el = document.getElementById("gameGrid");
+        elClone = el.cloneNode(true);
+        el.parentNode.replaceChild(elClone, el);
+        for(let i = 0; i<board.length; i++) {
+            let field = document.getElementById(i+1);
+            field.style.pointerEvents = "none";
+        }
+    }
+
+    const checkWin = () => {
+        if( (board[0] == "O" && board[3] == "O" && board[6] == "O") || 
+        (board[1] == "O" && board[4] == "O" && board[7] == "O") || 
+        (board[2] == "O" && board[5] == "O" && board[8] == "O") || 
+        (board[0] == "O" && board[1] == "O" && board[2] == "O") || 
+        (board[3] == "O" && board[4] == "O" && board[5] == "O") ||
+        (board[6] == "O" && board[7] == "O" && board[8] == "O") || 
+        (board[0] == "O" && board[4] == "O" && board[8] == "O") || 
+        (board[2] == "O" && board[4] == "O" && board[6] == "O") ) {   
+            DisplayController.showWinner("Player 2");
+            removeListeners();
+        } else if ( (board[0] == "X" && board[3] == "X" && board[6] == "X") || 
+        (board[1] == "X" && board[4] == "X" && board[7] == "X") || 
+        (board[2] == "X" && board[5] == "X" && board[8] == "X") || 
+        (board[0] == "X" && board[1] == "X" && board[2] == "X") || 
+        (board[3] == "X" && board[4] == "X" && board[5] == "X") ||
+        (board[6] == "X" && board[7] == "X" && board[8] == "X") || 
+        (board[0] == "X" && board[4] == "X" && board[8] == "X") || 
+        (board[2] == "X" && board[4] == "X" && board[6] == "X") ) {
+            DisplayController.showWinner("Player 1");
+            removeListeners();
+        } else if (!board.includes("")) {
+            DisplayController.showWinner("Nobody")
+        }
+    }
+    return {board, checkWin}
+})();
+
+
+const GameController = (() => {
+    let whosTurn = player1;
+
+    const manipulateField = (id) => {
+        let field = document.getElementById(id);
+        field.textContent = GameBoard.board[id-1];
+        console.log("successfully removed")
+        if(whosTurn.name == "Player 1") {
+            field.classList.add("made-red");
+            field.classList.remove("active");
+        } else {
+            field.classList.add("made-green")
+            field.classList.remove("active");
+        }
+    }
+
+    const makeMove = (id,board) => {
+        if(whosTurn.name == "Player 1" && GameBoard.board[id-1]=="") {
+            GameBoard.board[id-1] = "X";
+            whosTurn = player2;
+            manipulateField(id);
+        } else if(whosTurn.name == "Player 2" && GameBoard.board[id-1]=="") {
+            GameBoard.board[id-1] = "O";
+            whosTurn = player1;
+            manipulateField(id);
+        }
+        DisplayController.handleTurn(whosTurn.name);
+        GameBoard.checkWin();
+    };
+
+    const reset = () => {
+        console.log("RESET")
+    }
+    return {makeMove,whosTurn, reset};
+})();
+
+const DisplayController = (() => {
+    const display = document.getElementById("turn-indicator")
+    const handleTurn = (name) => {
+        display.textContent = "Current turn: " + name;
+    };
+
+    const showWinner = (name) => {
+        display.textContent = name + " won!";
+    };
+    return {handleTurn, showWinner};
+})();
+
+//ON INIT
+
+//GUI Elements
+const gameGrid = document.getElementById('gameGrid');
+
+
+//Load Board
+for(let i = 1; i<=GameBoard.board.length; i++) {
+    console.log(GameBoard.board[i-1])
+    let fieldDiv = document.createElement('div');
+    fieldDiv.id = i;
+    fieldDiv.classList.add('field');
+    fieldDiv.innerHTML = GameBoard.board[i-1];
+    fieldDiv.addEventListener('click', function _listener() {
+        GameController.makeMove(fieldDiv.id, GameBoard.board)
+        fieldDiv.removeEventListener('click', _listener)
+    });
+    gameGrid.appendChild(fieldDiv);
+}
+
+DisplayController.handleTurn("Player 1");
